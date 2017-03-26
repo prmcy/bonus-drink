f9cf7afcccebc9b9c64cc6864e234cc957582f88
prmcy <kb27inYttPrmcy@gmail.com>
2017-03-26 23:09:52 +0900

    modified spec file

--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -8,4 +8,28 @@ describe BonusDrink do
       is_expected.to eq 0
     end
   end
+  context '@amount == 1' do
+    before { @amount = 1 }
+    it 'total count is 1' do
+      is_expected.to eq 1
+    end
+  end
+  context '@amount == 3' do
+    before { @amount = 3 }
+    it 'total count is 4' do
+      is_expected.to eq 4
+    end
+  end
+  context '@amount == 11' do
+    before { @amount = 11 }
+    it 'total count is 16' do
+      is_expected.to eq 16
+    end
+  end
+  context '@amount == 100' do
+    before { @amount = 100 }
+    it 'total count is 149' do
+      is_expected.to eq 149
+    end
+  end
 end
