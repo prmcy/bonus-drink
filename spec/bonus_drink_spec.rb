980039011328e81d15fd7953c1adf911f3bec05e
prmcy <kb27inYttPrmcy@gmail.com>
2017-03-26 22:56:52 +0900

    modified spec file

--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -1,5 +1,11 @@
 require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
 
 describe BonusDrink do
-  specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
+  subject { BonusDrink.total_count_for(@amount) }
+  context '@amount == 0' do
+    before { @amount = 0 }
+    it 'total count is 0' do
+      is_expected.to eq 0
+    end
+  end
 end
