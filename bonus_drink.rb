d9c40d9d2bf4f2ceda7fdf35489d548e38d78909
prmcy <kb27inYttPrmcy@gmail.com>
2017-03-26 23:37:37 +0900

    Implement total_count_for

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,11 @@
 class BonusDrink
+  @@empty_bottle_num = 3
+  @@bonus_bottle_num = 1
   def self.total_count_for(amount)
-    0
+    if amount < @@empty_bottle_num then 
+      amount
+    else
+      @@empty_bottle_num + total_count_for(amount - @@empty_bottle_num + @@bonus_bottle_num)
+    end
   end
 end
